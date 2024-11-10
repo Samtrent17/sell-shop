5242975a94f98176faadaec3dc7a0a0b063383eb
Samtrent17 <xamsamuel079@gmail.com>
2024-11-10 10:00:57 +0300

    commit

--- sell-shop-backend/testing.js
+++ sell-shop-backend/testing.js
@@ -1,5 +1,6 @@
 console.log("Mongo URI:", process.env.MONGO_URI);
 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log('MongoDB connection error:', error));
